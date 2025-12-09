db40ae7bd0fcb12d7d33a019e6527acc88e7132e
dejauvkukky <dejavukukky@gmail.com>
2025-12-09 10:42:29 +0900

    Fix damaged firebase config file

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,17 @@
 // Firebase Configuration
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/setup#config-object
 const firebaseConfig = {
-    const db = firebase.firestore();
+    apiKey: "AIzaSyANrUXwBGvmbDeVF2eqTeCb8oXPNaBIIAk",
+    authDomain: "familly-fun-service.firebaseapp.com",
+    projectId: "familly-fun-service",
+    storageBucket: "familly-fun-service.firebasestorage.app",
+    messagingSenderId: "257202552832",
+    appId: "1:257202552832:web:add8b7eb7672889dbdd8e5"
+};
 
-    export { auth, db };
+// Initialize Firebase
+// Note: We use the CDN scripts in HTML, so 'firebase' global object is available
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+export { auth, db };
