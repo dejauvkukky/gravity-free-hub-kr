ecfed5eb9d7f1e21801c01178466bc7991e18dc7
dejauvkukky <dejavukukky@gmail.com>
2025-12-09 10:40:57 +0900

    Restore valid Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,18 +2,6 @@
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/setup#config-object
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
+    const db = firebase.firestore();
 
-// Initialize Firebase
-// Note: We use the CDN scripts in HTML, so 'firebase' global object is available
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
-
-export { auth, db };
+    export { auth, db };
