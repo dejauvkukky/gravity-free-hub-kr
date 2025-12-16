2582d21725af151596ffad3ca8418119a1b57971
dejauvkukky <dejavukukky@gmail.com>
2025-12-16 16:18:25 +0900

    style(all): fixed headers round 7 - dashboard gradient restored, subpage header visibility fixed v=202512161621

--- src/wishlist.js
+++ src/wishlist.js
@@ -1,5 +1,5 @@
-import './firebase.js?v=202512161615';
-import { customAlert, customConfirm } from './ui-utils.js?v=202512161615';
+import './firebase.js?v=202512161621';
+import { customAlert, customConfirm } from './ui-utils.js?v=202512161621';
 
 /* -------------------------------------------------------------------------- */
 /*                                  Constants                                 */
