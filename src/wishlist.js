2761ad692dddeb34ead29792982b4984f25e21e5
dejauvkukky <dejavukukky@gmail.com>
2025-12-16 15:57:11 +0900

    style(dashboard): refine title text effect and lift date visually v=202512161559

--- src/wishlist.js
+++ src/wishlist.js
@@ -1,5 +1,5 @@
-import './firebase.js?v=202512161550';
-import { customAlert, customConfirm } from './ui-utils.js?v=202512161550';
+import './firebase.js?v=202512161559';
+import { customAlert, customConfirm } from './ui-utils.js?v=202512161559';
 
 /* -------------------------------------------------------------------------- */
 /*                                  Constants                                 */
