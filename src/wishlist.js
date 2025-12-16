fe322916236d70b4a736cb3a43adae69265ede75
dejauvkukky <dejavukukky@gmail.com>
2025-12-16 15:23:08 +0900

    fix(wishlist): simplify load more button text v=202512161523

--- src/wishlist.js
+++ src/wishlist.js
@@ -1,5 +1,5 @@
-import './firebase.js?v=202512161519';
-import { customAlert, customConfirm } from './ui-utils.js?v=202512161519';
+import './firebase.js?v=202512161523';
+import { customAlert, customConfirm } from './ui-utils.js?v=202512161523';
 
 /* -------------------------------------------------------------------------- */
 /*                                  Constants                                 */
